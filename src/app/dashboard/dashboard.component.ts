6031b819533f4653cae851745465936fca8aa758
pedrohtbl <pedrohtbl@gmail.com>
2023-01-19 16:37:54 -0300

    3 files modified, 3 added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { dolar } from '../spark/data';
+import {
+  SparkSharedEventsService,
+} from '../spark/service/spark-shared-events.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,7 +10,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
+  public dataDolarQuotation = dolar;
+
+  constructor(private sparkSharedEventsService: SparkSharedEventsService) {}
+
   ngOnInit() {
+    this.sendData()
     console.log('dashboard');
+  }
+
+  public sendData() {
+    this.sparkSharedEventsService.sendClickEvent(this.dataDolarQuotation);
   }
 }
--- src/app/spark/spark.component.ts
+++ src/app/spark/spark.component.ts
@@ -1,15 +1,131 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexDataLabels,
+  ApexGrid,
+  ApexMarkers,
+  ApexResponsive,
+  ApexStroke,
+  ApexTitleSubtitle,
+  ApexTooltip,
+  ApexXAxis,
+  ChartComponent,
+} from 'ng-apexcharts';
+import { Subscription } from 'rxjs';
+import { dolar } from './data';
+import { SparkSharedEventsService } from './service/spark-shared-events.service';
+
+export interface ChartOptions {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  dataLabels: ApexDataLabels;
+  grid: ApexGrid;
+  stroke: ApexStroke;
+  title: ApexTitleSubtitle;
+  markers: ApexMarkers;
+  tooltip: ApexTooltip;
+  responsive: ApexResponsive[];
+  colors: any[];
+}
 
 @Component({
   selector: 'app-spark',
   templateUrl: './spark.component.html',
-  styleUrls: ['./spark.component.css']
+  styleUrls: ['./spark.component.css'],
 })
 export class SparkComponent implements OnInit {
+  public chartOptions: Partial<ChartOptions>;
+  public subscriptions: Subscription[] = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  @ViewChild('chart') chart: ChartComponent;
+  @Input() data: ApexAxisChartSeries;
+  @Input() name: string;
+  constructor(private sparkSharedEventsService: SparkSharedEventsService) {
+    this.chartOptions = {
+      series: this.data,
+      chart: {
+        height: 80,
+        type: 'line',
+        sparkline: {
+          enabled: true,
+        },
+        dropShadow: {
+          enabled: true,
+          top: 1,
+          left: 1,
+          blur: 2,
+          opacity: 0.2,
+        },
+      },
+      colors: ['#fff'],
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: 'smooth',
+      },
+      markers: {
+        size: 0,
+      },
+      grid: {
+        padding: {
+          top: 20,
+          bottom: 20,
+          left: 110,
+        },
+        row: {
+          colors: ['transparent'],
+          opacity: 0.5,
+        },
+      },
+      responsive: [
+        {
+          breakpoint: 1080,
+          options: {
+            grid: {
+              padding: {
+                top: 20,
+                bottom: 20,
+                left: 75,
+              },
+            },
+          },
+        },
+        {
+          breakpoint: 767,
+          options: {
+            grid: {
+              padding: {
+                top: 20,
+                bottom: 20,
+                left: 110,
+              },
+            },
+          },
+        },
+      ],
+      tooltip: {
+        x: {
+          show: true,
+        },
+        y: {
+          title: {
+            formatter: function formatter(val) {
+              return '';
+            },
+          },
+        },
+        theme: 'dark',
+      },
+      xaxis: {
+        type: 'datetime',
+      },
+    };
   }
 
+  ngOnInit() {
+    console.log(this.data);
+  }
 }
